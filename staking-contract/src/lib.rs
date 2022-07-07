3a97a81817216380af0013bc2c23b201a153d56e
chinhnd7 <ndchinh110797@gmail.com>
2022-07-07 17:43:54 +0700

    init project

---
+++ staking-contract/src/lib.rs
@@ -0,0 +1,14 @@
+use near_sdk::{env, AccountId, Balance, BlockHeight, EpochHeight}
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
